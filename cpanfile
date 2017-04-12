515885b178eee05779294e2828fb108dc15ed545
Jack Terry <jterry2@myseneca.ca>
2017-04-12 16:19:24 -0400

    update cpanfile

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
 requires "Template" => "0";
 requires "DBI" => "0";
 requires "CGI" => "0";
+requires "DBD::mysql"; => "0";
--- test.pl
+++ test.pl
@@ -1,12 +1,27 @@
 #!/usr/bin/perl
-use strict;
+use strict; 
 use Template;
 use DBI;
 use CGI;
-use Apache::Session::MySQL;
-use constant TEMPLATELIB => '/export/srv/www/vhosts/main/tt2';
 
-$cgiObject=new CGI; #$cgiObject=CGI->new();
+my $cgiObject=new CGI; #$cgiObject=CGI->new();
 print STDOUT $cgiObject->header('text/html');
 
 print "<p>hello world</p>";
+
+my $dbName = $ENV{'MYSQL_DATABASE'};
+my $user = $ENV{'MYSQL_USER'};
+my $dbPassword = $ENV{'MYSQL_PASSWORD'};
+my $host = $ENV{'MYSQL_HOST'};
+
+my $dsn = "DBI:mysql:$dbName:$host";
+
+my $dbObject = DBI->connect($dsn, $user, $dbPassword)
+or print ("Can't connect user $user to database $host:$dbName");
+my $query = "SELECT * from apps_countries";
+my $sth = $dbObject->prepare ($query);
+$sth->execute() or print "error executing";
+while (my $ref = $sth->fetchrow_hashref()) {
+	print $ref;
+}
+
