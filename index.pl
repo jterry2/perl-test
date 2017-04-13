6e48dbb944a31ad66b676f932fe78fc034298943
Jack Terry <jterry2@myseneca.ca>
2017-04-13 09:04:06 -0400

    added displaying city population

--- index.pl
+++ index.pl
@@ -1,3 +1,4 @@
 #!/usr/bin/perl
 #
+print "content-type: text/html\n\n";
 print "hello world";
--- pop.pl
+++ pop.pl
@@ -3,7 +3,7 @@ use Template;
 use DBI;
 use CGI;
 
-use constant TEMPLATE => 'results.tt2';
+use constant TEMPLATE => 'pop.tt2';
 use constant TEMPLATELIB => '.';
 
 my %config=(
@@ -12,7 +12,7 @@ my %config=(
 
 my $ttObject = Template->new( \%config );
 
-my $cgiObject=new CGI; 
+my $cgiObject=new CGI;
 print STDOUT $cgiObject->header('text/html');
 
 my $dbName = $ENV{'MYSQL_DATABASE'};
@@ -25,17 +25,20 @@ my $dsn = "DBI:mysql:$dbName:$host";
 my $dbObject = DBI->connect($dsn, $user, $dbPassword)
 or print ("Can't connect user $user to database $host:$dbName");
 
-my $query = "SELECT country.Name, city.Name AS Capital, city.ID AS ID FROM country LEFT JOIN city ON country.Capital = city.ID";
+my $city = $ENV{'QUERY_STRING'};
+$city =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+
+my $query = "SELECT Name, Population FROM city WHERE ID = $city";
+
 my $sth = $dbObject->prepare ($query);
 $sth->execute() or print "error executing";
-
 my @table;
 while (my $ref = $sth->fetchrow_hashref()) {
         push(@table, $ref);
 }
 
 my $vars = {
-        countries => \@table
+        cities => \@table
 };
 
 $ttObject->process ( TEMPLATE, $vars )
